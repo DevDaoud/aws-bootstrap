cffe002df06e3b054d056903010291d5ae6106bf
Your Name <you@example.com>
2024-03-11 17:40:09 +0100

    Added spec

---
+++ deploy_infra.sh
@@ -0,0 +1,32 @@
+STACK_NAME=awsbootstrap 
+REGION=eu-north-1 
+CLI_PROFILE=awsbootstrap
+
+EC2_INSTANCE_TYPE=t3.micro 
+
+# Deploy the CloudFormation template
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides Ec2InstanceType=$EC2_INSTANCE_TYPE
+
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+echo -e "\n\n=========== Deploying setup.yml ==========="
+
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
