1c58e7f79f4bf4ab6c2aa6eb3669f4d83d25ee2a
Your Name <you@example.com>
2024-03-12 23:27:10 +0100

    New code

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -40,7 +40,10 @@ aws cloudformation deploy \
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
+  # aws cloudformation list-exports \
+  #   --profile awsbootstrap \
+  #   --query "Exports[?Name=='InstanceEndpoint'].Value" 
   aws cloudformation list-exports \
     --profile awsbootstrap \
-    --query "Exports[?Name=='InstanceEndpoint'].Value" 
+    --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
 fi
